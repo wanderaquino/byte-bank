b4d455c2c4edc39cf332799d407ecd0e7feea1eb
Wander Aquino <wander.aquino@gmail.com>
2022-01-21 23:35:12 -0300

    Switch  to ContainerWrapper to export

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
-import Container from "./Components/Container";
+import ContainerWrapper from "./Components/Container";
 
 import { GlobalStyle } from "./Components/GlobalStyle";
 
@@ -10,7 +10,7 @@ function App() {
     <>
     <GlobalStyle />
     <Cabecalho />
-    <Container />
+    <ContainerWrapper />
     </>
   );
 }
