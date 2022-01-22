33b04fa9f877fa4aad2fecb6cf4c743cfcb3e408
Wander Aquino <wander.aquino@gmail.com>
2022-01-21 23:33:57 -0300

    Declare Container and Conteudo styled component

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
+
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ContainerWrapper = () => {
   return (
     <div className="container">
       <Titulo>Olá Fulano!</Titulo>
