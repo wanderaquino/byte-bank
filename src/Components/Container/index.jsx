ea12c9070c0411bab062932eda4a358e23296b3e
Wander Aquino <wander.aquino@gmail.com>
2022-01-23 00:04:10 -0300

    Increase media query from Conteudo component

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -14,6 +14,11 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+
 `
 
 const ContainerWrapper = () => {
