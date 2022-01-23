97f47f7e2447130b4bf66c22f418ef2f09eeba87
Wander Aquino <wander.aquino@gmail.com>
2022-01-22 23:44:00 -0300

    Styled button and Box component

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -11,6 +11,34 @@ const IconeMarginTop = styled(Icone) `
   margin-top: 2px
 
 `
+
+const Box = styled.div `
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.04);
+  padding: 20px;
+  width: 48%;
+
+`
+
+const ContaButton = styled.button `
+
+  margin: 15px auto 0px auto;
+  display: block;
+  border-radius: 20px;
+  background-color: #41d3be;
+  border: none;
+  color: white;
+  font-weight: 600;
+  font-size: 14px;
+  padding: 8px 20px;
+  cursor: pointer;
+
+`
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
 
