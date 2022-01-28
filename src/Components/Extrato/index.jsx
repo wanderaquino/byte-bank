329e0fee7d38fe81e7b111e9bbe8404dd185b698
Wander Aquino <wander.aquino@gmail.com>
2022-01-27 23:29:07 -0300

    Track new component Extrato

---
+++ src/Components/Extrato/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Box, ContaButton} from "../../UI/index";
+
+export function Extrato () {
+    return (
+        <Box>
+
+            <ContaButton>Ver Mais</ContaButton>
+        </Box>
+    )
+}
