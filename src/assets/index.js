5dc6e9471963eb113fbea1d8a26380188e4c410e
Wander Aquino <wander.aquino@gmail.com>
2022-01-31 00:39:30 -0300

    Track js from icons assets

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import alimentacaoIcone from "../assets/images/alimentacao.svg";
+import outrosIcone from "../assets/images/outros.svg";
+import saudeIcone from "../assets/images/saude.svg";
+import utilidadesIcone from "../assets/images/utilidades.svg";
+import transporteIcone from "../assets/images/transporte.svg";
+
+export const categorias = {
+    Restaurante: alimentacaoIcone,
+    Saude: saudeIcone,
+    Outros: outrosIcone,
+    Utilidades: utilidadesIcone,
+    Transporte: transporteIcone
+}
