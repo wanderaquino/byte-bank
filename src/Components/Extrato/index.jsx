296c886e08cfc4f1f50c5fddfdd2798b501da496
Wander Aquino <wander.aquino@gmail.com>
2022-01-31 00:36:25 -0300

    Add useEffect for fetch data from updates

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Box, ContaButton} from "../../UI/index";
+import {api} from "../../services/api";
+import { LinhaExtrato } from "../LinhaExtrato";
 
 export function Extrato () {
+
+    const [updates, setUpdates] = useState([{}]);
+    const [isWaiting, setIsWaiting] = useState(true);
+
+
+    useEffect(() => {
+        async function fetchUpdates() {
+            const response =  await api.get("/updates");
+            setUpdates(response.data);
+            setIsWaiting(false);
+        };
+        fetchUpdates();
+
+        // setUpdates(response);
+        }, []
+    );
+
     return (
         <Box>
 
