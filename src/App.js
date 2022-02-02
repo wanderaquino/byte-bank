7aa76d819a6b83fbbccc09fea3cd872820e465f3
Wander Aquino <wander.aquino@gmail.com>
2022-02-02 00:47:19 -0300

    Use theme switcher for dark / light

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import ContainerWrapper from "./Components/Container";
@@ -6,13 +6,23 @@ import ContainerWrapper from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
 import {ThemeProvider} from "styled-components";
 import {darkTheme, lightTheme} from "./UI/themes";
+import {ThemeSwitch} from "./Components/ThemeSwitch";
+
 
 function App() {
+
+  const [isLightTheme, setIsLightTheme] = useState(true);
+
+  function handleClickThemeButton () {
+    setIsLightTheme(!isLightTheme);
+  }
+
   return (
     <>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={isLightTheme? lightTheme : darkTheme}>
       <GlobalStyle />
       <Cabecalho />
+      <ThemeSwitch  onClickFunction={handleClickThemeButton} theme={isLightTheme} />
       <ContainerWrapper />
     </ThemeProvider>
     </>
