fb51e01039c5b237791e4cb5ab2ab629eeb6b94b
Wander Aquino <wander.aquino@gmail.com>
2022-02-01 23:49:51 -0300

    Track new file theme.js

---
+++ src/UI/themes.js
@@ -0,0 +1,13 @@
+import { lightBackground, lightBoxInsideBackground, lightTextColor, darkBackground, darkTextColor, darkBoxInsideBackground } from "./index"; 
+
+export const darkTheme = {
+    body: darkBackground,
+    inside: darkBoxInsideBackground,
+    text: darkTextColor
+}
+
+export const lightTheme = {
+    body: lightBackground,
+    inside: lightBoxInsideBackground,
+    text: lightTextColor
+}
